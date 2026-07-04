3753fb91cec3980667145dd113561a631ec78801
goudarooni <k@gankiesel.email>
2026-07-04 15:48:37 -0500

    file name fix in layout.tsx, site running on local

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
-import "./global.css";
+import "./globals.css";
 
 const openSans = Open_Sans({
     subsets: ["latin"],
