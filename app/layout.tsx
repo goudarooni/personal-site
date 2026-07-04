73a20f63bd883715bc2a04d1df1853f97d90e0bb
goudarooni <k@gankiesel.email>
2026-07-04 14:54:38 -0500

    font, links, and photo add

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./global.css";
+
+const openSans = Open_Sans({
+    subsets: ["latin"]
+    weight: ["400", "700"]
+    display: "swap",
+    variable: "--font-open-sans"
+});
+
+export const metadata: Metadata = {
+    title: "Kagan Kiesel"
+    description: personal site
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en" className={openSans.variable}>
+            <body>{children}</body>
+        </html>
+    );
+}
