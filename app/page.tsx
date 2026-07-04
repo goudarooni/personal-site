25879ccddb1d21afa743c7686df93dac113425b5
goudarooni <k@gankiesel.email>
2026-07-04 15:38:44 -0500

    css page update

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+export default function Home() {
+  return (
+    <main className="home">
+      <div className="home__main">
+        <h1 className="home__name">Kagan Kiesel</h1>
+
+        <ul className="home__links">
+          <li>
+            <a href="mailto:kagan@kagankiesel.com">Email</a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/@kagankiesel"
+              target="_blank"
+              rel="noreferrer"
+            >
+              YouTube
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <figure className="home__photo-wrap">
+        <img className="home__photo" src="/site.jpg" alt="Kagan Kiesel" />
+        <figcaption className="home__caption">
+          Andy Warhol, <em>Pelé</em>, 1977, Polaroid
+          <br />
+          4 1/4 × 3 3/8 in | 10.8 × 8.6 cm
+          <br />
+          Brooklyn Museum, New York City, New York, USA
+        </figcaption>
+      </figure>
+    </main>
+  );
+}
