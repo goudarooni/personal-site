60ce97b8fb4a12a0f3817f2c7e2199e465e27984
goudarooni <k@gankiesel.email>
2026-08-15 16:21:45 -0500

    under construction wording fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,40 +2,11 @@ export default function Home() {
   return (
     <main className="home">
       <div className="home__main">
-        <h1 className="home__name">kagan kiesel</h1>
+        <h1 className="home__name">(under construction)</h1>
 
         <ul className="home__links">
           <li>
             <a href="mailto:kagan@kagan.ooo">email</a>
-          </li>
-
-          <li>
-            <details className="home__menu">
-              <summary>projects</summary>
-              <ul className="home__submenu">
-                <li>
-                  <a href="https://www.youtube.com/@kagankiesel">youtube</a>
-                </li>
-                <li>
-                  <a href="https://www.kagankiesel.substack.com">journal</a>
-                </li>
-                <li>
-                  <details className="home__menu">
-                    <summary>hopper</summary>
-                    <ul className="home__submenu">
-                      <li>
-                        <a href="https://hopper.kagan.ooo">hopper</a>
-                      </li>
-                      <li>
-                        <a href="https://github.com/goudarooni/hopper-sun">
-                          github
-                        </a>
-                      </li>
-                    </ul>
-                  </details>
-                </li>
-              </ul>
-            </details>
           </li>
         </ul>
       </div>
